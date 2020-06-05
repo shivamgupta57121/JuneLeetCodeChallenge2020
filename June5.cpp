946f2d779327bc1b2552bd25a8f9577d5c790a5a
SHIVAM GUPTA <31137037+shivamgupta57121@users.noreply.github.com>
2020-06-05 23:57:37 +0530

    Random Pick with Weight

---
+++ June5.cpp
@@ -0,0 +1,36 @@
+// Problem: Random Pick with Weight
+// Link: https://leetcode.com/problems/random-pick-with-weight/
+
+// Solution: 
+class Solution {
+public:
+    vector<int> v;
+    Solution(vector<int>& w) {
+        v.push_back(w[0]);
+        int total=w[0];
+        for(int i=1;i<w.size();i++){
+            total = total + w[i];
+            v.push_back(total);
+        }
+    }
+    
+    int pickIndex() {
+        int val = rand() %v[v.size()-1] +1;
+        
+        // Linear Search
+        /*
+        int i=0;
+        while(val>v[i]) i++;
+        return i;
+        */
+        
+        // Binary Search
+        int start=0,end=v.size()-1;
+        while(start<end){
+            int mid = start + (end-start)/2;
+            if(val>v[mid]) start = mid+1;
+            else end = mid;
+        }
+        return start;
+    }
+};
