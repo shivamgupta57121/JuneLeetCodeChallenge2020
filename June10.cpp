78d7fb38e6b5247f9405663bf3bfaab224f5e030
SHIVAM GUPTA <31137037+shivamgupta57121@users.noreply.github.com>
2020-06-11 00:04:34 +0530

    Search Insert Position

--- June10.cpp
+++ June10.cpp
@@ -14,3 +14,21 @@ public:
         return i;
     }
 };
+
+// Binary Search : O(log(n))
+class Solution {
+public:
+    int searchInsert(vector<int>& A, int target) {
+        int n = A.size();
+        int start = 0, end = A.size()-1;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(target==A[mid]) return mid;
+            else if(target>A[mid]) start = mid+1;
+            else {
+                end = mid-1;
+            }
+        }
+        return start;
+    }
+};
